e0a33c87e5eb5f79e15ad368dc6ec289db210bbd
Sneha-T8015 <106481080+Sneha-T8015@users.noreply.github.com>
2023-10-31 21:14:24 +0530

    first commit

---
+++ devtown-react/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       Heeloo
+      </header>
+    </div>
+  );
+}
+
+export default App;
